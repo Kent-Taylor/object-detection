b1f614c88437dd56ceb27a30b1cdc3188d2acabb
107124 <kredfox4@gmail.com>
2022-02-24 15:27:04 -0500

    i added speech and created a join function so that the sentence sounds more smooth and logical when said outloud.

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 # pip install opencv-contrib-python
 # pip install cvlib
+# # pip install gtts
+# # pip install playsound
+# use `pip3 install PyObjC` if you want playsound to run more efficiently.
 
 import cv2
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
+from gtts import gTTS
+from playsound import playsound
+
+
+def speech(text):
+    print(text)
+    language = "en"
+    output = gTTS(text=text, lang=language, slow=False)
+
+    output.save("./sounds/output.mp3")
+    playsound("./sounds/output.mp3")
+
 
 video = cv2.VideoCapture(1)
 labels = []
@@ -26,11 +41,14 @@ while True:
         break
 
 i = 0
+new_sentence = []
 for label in labels:
     if i == 0:
-        print(f"I found a {label}, and ")
+        new_sentence.append(f"I found a {label}, and, ")
     else:
-        print(f"a {label}")
+        new_sentence.append(f"a {label},")
 
     i += 1
+
+speech(" ".join(new_sentence))
 
