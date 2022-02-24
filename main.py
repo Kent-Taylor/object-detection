7672ed2ae778ff16026e0844dadd71d6a9bcefe4
107124 <kredfox4@gmail.com>
2022-02-24 14:37:28 -0500

    I figured out how to append each of the items the camera was able to see and add them to a list without duplicates. I created string interpolation to say each item logically. Next step is to get the program to speak it outloud

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 # pip install opencv-contrib-python
-# pip install matplotlib
 # pip install cvlib
 
 import cv2
-#
-import matplotlib.pyplot as plt
-#
-# from cvlib import detect_common_objects
-#
-# from cvlib.object_detection import draw_bbox
-
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
-# img = cv2.imread("cars.jpeg")
-# img = cv2.imread("fruit.jpeg")
-video = cv2.VideoCapture(1)
 
-end_points = {
-    "person": 0.9762513041496277,
-    "orange": 0.9320018887519836,
-    "apple": 0.5922269225120544,
-    "book": 0.5326956510543823
-}
+video = cv2.VideoCapture(1)
+labels = []
 
 while True:
     ret, frame = video.read()
@@ -31,31 +16,21 @@ while True:
 
     cv2.imshow("Detection", output_image)
 
-    labels = []
     for item in label:
-        labels.append(item)
+        if item in labels:
+            pass
+        else:
+            labels.append(item)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
+i = 0
 for label in labels:
-    print(label)
-    # i = 0
-    # i += 1
-    # if i == 1:
-    #     print(f"I found a {label}, and ")
-    # else:
-    #     print(f"a {label}")
+    if i == 0:
+        print(f"I found a {label}, and ")
+    else:
+        print(f"a {label}")
 
+    i += 1
 
-# print(output_image)
-
-# bbox, label, conf = detect_common_objects(img)
-#
-# output_image = draw_bbox(img, bbox, label, conf)
-#
-# plt.imshow(output_image)
-#
-# plt.show()
-
-# print(f"Number of cars: {label.count}")
